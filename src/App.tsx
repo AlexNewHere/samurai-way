4f7792bd4e818b8d2a09e96b1c44b4d1bf24e876
Alex <tendren3310@googlemail.com>
2022-03-29 10:44:00 +0300

    2

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import './App.css';
+import {Header} from "./Header";
+import {Technologies} from './Technologies';
 
-function App() {
-  return (
-    <div className="App">
-     amurai! Let's go!
-    </div>
-  );
+const App = () => {
+    return (
+        <div>
+            <Header />
+            <Technologies />
+        </div>
+    );
 }
 
 export default App;
+
