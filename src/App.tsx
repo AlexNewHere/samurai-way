6ec10dee3b30a243b975629e8d3bf4805213d397
Alex <tendren3310@googlemail.com>
2022-04-06 21:55:22 +0300

    Lesoon 20 NavLink

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,25 @@ import {Navbar} from './components/Navbar/Navbar';
 import {Content} from './components/Content/Content';
 import {Footer} from './components/Footer/Footer';
 import {Dialogs} from './components/Dialog/Dialogs';
+import {Route, Routes} from 'react-router-dom';
 
-const App = () => {
+
+export const App = () => {
     return (
         <div className="grid_wrapper">
             <Header/>
             <Navbar/>
             {/*<Content/>*/}
-            <div className='grid_wrapper_content'>
-                <Dialogs/>
+            <div className="grid_wrapper_content">
+
+                <Routes>
+                    <Route path='/dialogs' element={<Dialogs/>}/>
+                    <Route path='/content' element={<Content/>}/>
+                </Routes>
             </div>
-            <Footer/>
+                    <Footer/>
         </div>
     );
 }
 
-export default App;
 
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
 import s from './Navbar.module.css'
+import {NavLink} from 'react-router-dom';
 
+const linkActive = ({isActive})=>isActive? s.active: s.link;
 
 export const Navbar = () =>{
     return (
         <div className={s.sidebar}>
             <div className={s.item}>
-                <a href='/dialogs'>Profile</a>
+                <NavLink to='/dialogs' className={linkActive}>Profile</NavLink>
             </div>
             <div className={s.item}>
-                <a>Messages</a>
+                <NavLink to='/content' className={linkActive}>Message</NavLink>
             </div>
             <div className={s.item}>
-                <a>News</a>
+                <NavLink to='/content' className={linkActive}>News</NavLink>
             </div>
             <div className={s.item}>
-                <a>Music</a>
+                <NavLink to='/content' className={linkActive}>Music</NavLink>
             </div>
             <div className={s.item}>
-                <a>Settings</a>
+                <NavLink to='/content' className={linkActive}>Settings</NavLink>
             </div>
         </div>
     )
